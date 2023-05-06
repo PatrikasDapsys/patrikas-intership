ca370ea3679ac7c7c17a2a1d111f91c5b6703139
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-05-06 21:59:14 +0300

    added a skeleton phase to author page(Author items not included)

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link, useParams } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
 import axios from "axios";
 
 const Author = () => {
@@ -10,29 +9,33 @@ const Author = () => {
   const [authorData, setAuthorData] = useState([]);
   const [isFollowButtonClicked, setIsFollowButtonClicked] = useState(false);
   const [followButtonText, setFollowButtonText] = useState("Follow");
+  const [Loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
+      setLoading(false);
       const { data } = await axios.get(`
       https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${authorID}
       `);
       setAuthorData(data);
+      setLoading(true);
     }
 
     fetchData();
-  }, []);
+  }, [authorID]);
 
   function clickedFollowButton() {
     setIsFollowButtonClicked(!isFollowButtonClicked);
     if (!isFollowButtonClicked) {
       authorData.followers += 1;
       setFollowButtonText("Unfollow");
-    }
-    else {
+    } else {
       authorData.followers -= 1;
       setFollowButtonText("Follow");
     }
-  } 
+  }
+
+  console.log(authorData);
 
   return (
     <div id="wrapper">
@@ -53,32 +56,84 @@ const Author = () => {
               <div className="col-md-12">
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={authorData.authorImage} alt="" />
+                    {Loading ? (
+                      <div className="profile_avatar">
+                        <img src={authorData.authorImage} alt="" />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          {authorData.authorName}
-                          <span className="profile_username">@{authorData.tag}</span>
-                          <span id="wallet" className="profile_wallet">
-                            {authorData.address}
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            {authorData.authorName}
+                            <span className="profile_username">
+                              @{authorData.tag}
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              {authorData.address}
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="profile_avatar">
+                        <div className="profile_avatar--skeleton skeleton-box"></div>
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4
+                            style={{ display: "flex", flexDirection: "column" }}
+                          >
+                            <span
+                              className="skeleton-box"
+                              style={{
+                                width: "200px",
+                                height: "24px",
+                                marginBottom: "8px",
+                              }}
+                            ></span>
+                            <span
+                              className="skeleton-box"
+                              style={{
+                                width: "100px",
+                                height: "16px",
+                                marginBottom: "8px",
+                              }}
+                            ></span>
+                            <span
+                              className="skeleton-box"
+                              style={{ width: "100px", height: "16px" }}
+                            ></span>
+                          </h4>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  {Loading ? (
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          {authorData.followers} followers
+                        </div>
+                        <Link
+                          to="#"
+                          className="btn-main"
+                          onClick={clickedFollowButton}
+                        >
+                          {followButtonText}
+                        </Link>
                       </div>
                     </div>
-                  </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">{authorData.followers} followers</div>
-                      <Link to="#" className="btn-main" onClick={clickedFollowButton}>
-                        {followButtonText}
-                      </Link>
+                  ) : (
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <span
+                          className="skeleton-box"
+                          style={{ width: "150px", height: "40px" }}
+                        ></span>
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
 
