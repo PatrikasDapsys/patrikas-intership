7d3d564d682862a10f948c9cfb93db18d7c63bf2
PatrikasDapsys <patrikas.dapsys@gmail.com>
2023-05-07 19:21:27 +0300

    removed extra key in map

--- src/components/author/AuthorItems.jsx
+++ src/components/author/AuthorItems.jsx
@@ -13,7 +13,6 @@ const AuthorItems = ({ authorData }) => {
                   key={elem.id}
                 >
                   <NftCard
-                    key={elem.id}
                     authorId={authorData.authorId}
                     authorImage={authorData.authorImage}
                     nftImage={elem.nftImage}
@@ -49,48 +48,3 @@ const AuthorItems = ({ authorData }) => {
 };
 
 export default AuthorItems;
-
-{
-  /* <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={elem.id}>
-  <div className="nft__item">
-    <div className="author_list_pp">
-      <Link to="">
-        <img className="lazy" src={authorData.authorImage} alt="" />
-        <i className="fa fa-check"></i>
-      </Link>
-    </div>
-    <div className="nft__item_wrap">
-      <div className="nft__item_extra">
-        <div className="nft__item_buttons">
-          <button>Buy Now</button>
-          <div className="nft__item_share">
-            <h4>Share</h4>
-            <a href="" target="_blank" rel="noreferrer">
-              <i className="fa fa-facebook fa-lg"></i>
-            </a>
-            <a href="" target="_blank" rel="noreferrer">
-              <i className="fa fa-twitter fa-lg"></i>
-            </a>
-            <a href="">
-              <i className="fa fa-envelope fa-lg"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-      <Link to="/item-details">
-        <img src={elem.nftImage} className="lazy nft__item_preview" alt="" />
-      </Link>
-    </div>
-    <div className="nft__item_info">
-      <Link to="/item-details">
-        <h4>{elem.title}</h4>
-      </Link>
-      <div className="nft__item_price">{elem.price} ETH</div>
-      <div className="nft__item_like">
-        <i className="fa fa-heart"></i>
-        <span>{elem.likes}</span>
-      </div>
-    </div>
-  </div>
-</div>; */
-}
